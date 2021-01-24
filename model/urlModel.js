eb502398035066946afbced73a7f6a9d4828f54f
banaulikarriya <riyabanaulikar@gmail.com>
2021-01-24 08:33:35 +0530

    project setup and database setup

---
+++ model/urlModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const taskSchema = new Schema({
+    url: {
+        type: String,
+        required: true,
+    },
+    shortcode: {
+        type: String,
+        required: true,
+    },
+    details: {
+        type: String,
+        default: ''
+    },
+    status: {
+        type: Boolean,
+        default: true,
+    },
+}, { timestamps: true }, );
+
+module.exports = mongoose.model('Urls', taskSchema);
---
+++ routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express();
+
+var taskController = require('../controller/shortcodeController');
+
+
+
+
+
+module.exports = router;
